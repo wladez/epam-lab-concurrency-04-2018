7b085cebc022a2e4963d5b7323b9ab263100de42
Vladislav Bushin <Vladislav_Bushin@epam.com>
2018-04-12 13:42:32 +0300

    Added impl of BlockingStack

---
+++ src/main/java/lesson_11_04_2018/stack/BlockingStack.java
@@ -0,0 +1,4 @@
+package lesson_11_04_2018.stack;
+
+public class BlockingStack {
+}
---
+++ src/main/java/lesson_11_04_2018/stack/Launcher.java
@@ -0,0 +1,37 @@
+package lesson_11_04_2018.stack;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        BlockingStack<Integer> stack = new BlockingStack<>(5);
+        ExecutorService service = Executors.newFixedThreadPool(15);
+
+        service.submit(() -> {
+            for (int i = 0; i < 7; i++) {
+                try {
+                    stack.push(i);
+                    System.out.println("Put in stack " + i);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        service.submit(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    System.out.println("Get from stack " + stack.pop());
+                    TimeUnit.MILLISECONDS.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        service.shutdown();
+    }
+}
